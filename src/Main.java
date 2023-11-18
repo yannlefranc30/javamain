1c2dda4283bf2cdcad41fc4a771955704ad1c1d6
user <yannlefranc330@gmail.com>
2023-11-18 13:45:17 +0100

    new

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.printf("bien il est temps de passer à autre chose!");
+
+
+        for (int i = 1; i <= 5; i++) {
+
+
+            System.out.println("i = " + i);
+        }
+    }
+}
